9582ac0f8ec482e4cbce3667f61db4ed46f35278
juanrocha <aguilajk@gmail.com>
2023-04-10 14:36:19 +0200

    draft matching comps

--- 13-Lloyds.R
+++ 13-Lloyds.R
@@ -6,12 +6,13 @@ library(tictoc)
 
 
 load("data/boats.Rda")
+load("data/vessels/vessels-1001-2000.RData")
 df_boats
 
 
 ## Initialize remote driver
 d <- rsDriver(
-    port = 4445L, browser = "firefox") # should open a chrome
+    port = 4440L, browser = "firefox") # should open a chrome
 remDr <- d[["client"]]
 #remDr$open()
 tic()
@@ -27,27 +28,72 @@ usr$sendKeysToElement(list("frida.bengtsson@su.se"))
 
 pwd <- remDr$findElement("id", 'thePage:siteTemplate:j_id27:password')
 pwd$sendKeysToElement(list(keyring::key_get("maritime", "frida.bengtsson@su.se"), key = "enter"))
-Sys.sleep(30)
+Sys.sleep(10)
 
 btn <- remDr$findElement("css", '.lli-nav__link')
 btn$clickElement()
 
-srch <- remDr$findElement("class", "lli-searchform__input")
-srch$clickElement()
-srch$sendKeysToElement(list(df_boats$boats[[1]] |> as.character()))
-#srch$sendKeysToElement(list(key = "down",key = "enter"))
-# gets the options from the list that appears on the search pane
-opts <- remDr$findElement("class","lli-typeahead-options__option")
-opts$clickElement() # click the first one
+# owners <- list()
+# features <- list()
 
-## Get html of all info on vessel:
-html <- remDr$getPageSource() |> 
-    unlist() |> 
-    read_html()
+remDr$refresh()
+Sys.sleep(5)
 
-tbls <- html |> html_elements("table") 
-info <- tbls[1] |> html_table()
+j = 1000 # last iteration in the last saved file.
 
+tic()
+for (i in 860:1000){
+    
+    srch <- remDr$findElement("class", "lli-searchform__input")
+    srch$clickElement()
+    # delete any left over from last iteration
+    walk(1:9, ~srch$sendKeysToElement(list(key = "backspace")))
+    
+    srch$sendKeysToElement(list(df_boats$boats[[i + j]] |> as.character()))
+    # gets the options from the list that appears on the search pane
+    Sys.sleep(15)
+    
+    opts <- remDr$findElement("class","lli-typeahead-options__option")
+    
+    ## Test if the vessel is found, if not go to next
+    if("No Results Found" == opts$getElementText() |> unlist()) {
+        walk(1:9, ~srch$sendKeysToElement(list(key = "backspace")))
+        next
+        }
+           
+    opts$clickElement() # click the first one)
 
+    Sys.sleep(20)
+    
+    ## Get html of all info on vessel:
+    html <- remDr$getPageSource() |> 
+        unlist() |> 
+        read_html()
+    # this captures the table with num of beneficial owners
+    tbls <- html |> html_elements("table") 
+    owners[[i]] <-  tbls[1] |> html_table() 
+    
+    f <- html |> html_elements(".mb1") |> 
+        html_elements("dl") |> 
+        html_elements("dt") |> 
+        html_text2()
+    v <- html |> html_elements(".mb1") |> 
+        html_elements("dl") |> 
+        html_elements("dd") |> 
+        html_text2()
 
-remDr$goBack()
+    features[[i]] <- tibble(
+        feature = f,
+        values = v[1:length(f)])
+    
+    rm(f,v,tbls) # clean up
+    print(glue::glue("Vessel", {i}, "done, next...", .sep = " "))
+    
+    remDr$goBack()
+    Sys.sleep(15)
+
+}
+toc() #30min for 40+ records.
+
+save(owners, features, file = "data/vessels/vessels-1001-2000.RData")
+
